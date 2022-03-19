65307ef25257abc0f21b6c88535db9b1515dc0ef
EvanCobb <evan.cobb@protonmail.com>
2022-03-19 15:12:12 -0500

    highlights your key level so you can tell what you'd get

--- Broker_MythicPlusRewards.lua
+++ Broker_MythicPlusRewards.lua
@@ -7,10 +7,7 @@ local highest_vault, highest_run = C_MythicPlus.GetRewardLevelForDifficultyLevel
 local green_cutoff = (highest_run - lowest_run) / 2 + lowest_run
 local purple_cutoff = (highest_vault - highest_run) * 2 / 3 + highest_run
 
-print(green_cutoff, purple_cutoff)
-
 local function color_cell(self, col, level)
-
     if level >= purple_cutoff then
         self:SetCellTextColor(self:GetLineCount(), col, 1, 0.5, 0, 1)
         return
@@ -31,18 +28,26 @@ local function color_cell(self, col, level)
         self:SetCellTextColor(self:GetLineCount(), col, 0.12, 1, 0, 1)
         return
     end
-
 end
 
 local function build_tooltip(self)
     self:AddHeader("Key", "Run", "Vault")
     self:AddSeparator()
 
+    local my_key_level = C_MythicPlus.GetOwnedKeystoneLevel()
+
     for key_level = 1, 15, 1 do
         local vault_level, run_level = C_MythicPlus.GetRewardLevelForDifficultyLevel(key_level)
         self:AddLine(key_level, run_level, vault_level)
         color_cell(self, 2, run_level)
         color_cell(self, 3, vault_level)
+
+        if my_key_level == key_level then
+            self:SetCellColor(self:GetLineCount(), 1, 0, 1, 0, 0.5)
+        end
+    end
+    if  my_key_level > 15 then
+        self:SetCellColor(self:GetLineCount(), 1, 0, 1, 0, 0.5)
     end
 end
 
